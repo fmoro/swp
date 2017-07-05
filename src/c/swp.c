c7393f7a6d1769a0ddee4130c5fff089b692041b
Federico Moro del Álamo <>
2017-07-05 18:58:07 +0200

    Initial commit

---
+++ src/c/swp.c
@@ -0,0 +1,159 @@
+#include <pebble.h>
+
+#ifndef MESSAGE_SOURCES
+#define MESSAGE_SOURCES
+#define SOURCE_FOREGROUND 0
+#define SOURCE_BACKGROUND 1
+#endif
+
+#ifndef LOGGING_TAG
+#define LOGGING_TAG 0xFEDE1983
+#endif
+
+static Window *s_window;
+
+static StatusBarLayer *s_status_bar;
+
+static ActionBarLayer *s_action_bar;
+static GBitmap *s_up_bitmap, *s_down_bitmap, *s_check_bitmap;
+
+static TextLayer *s_text_layer;
+
+static void worker_message_handler(uint16_t type, AppWorkerMessage *message) {
+  if (type == SOURCE_BACKGROUND) {
+    if (message->data0 == 0x0000 && message->data1 == 0x0000 && message->data2 == 0x0000) {
+      text_layer_set_text(s_text_layer, "Worker really stoped");
+    } else if (message->data0 == 0xFFFF && message->data1 == 0xFFFF && message->data2 == 0xFFFF) {
+      text_layer_set_text(s_text_layer, "Worker really started");
+    }
+  }
+}
+
+static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(s_text_layer, "Select");
+}
+
+static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(s_text_layer, "Up");
+  AppWorkerResult result = app_worker_launch();
+  if (result == APP_WORKER_RESULT_NO_WORKER) {
+    text_layer_set_text(s_text_layer, "No worker found");
+  } else if (result == APP_WORKER_RESULT_ASKING_CONFIRMATION) {
+    text_layer_set_text(s_text_layer, "Confirmation needed");
+  } else {
+    text_layer_set_text(s_text_layer, "Worker running");
+  }
+}
+
+static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(s_text_layer, "Down");
+  AppWorkerResult result = app_worker_kill();
+  if (result == APP_WORKER_RESULT_SUCCESS) {
+    text_layer_set_text(s_text_layer, "Worker stoped");
+  } else if (result == APP_WORKER_RESULT_DIFFERENT_APP) {
+    text_layer_set_text(s_text_layer, "Different app worker");
+  } else if (result == APP_WORKER_RESULT_NOT_RUNNING){
+    text_layer_set_text(s_text_layer, "Worker already stoped");
+  }
+}
+
+static void prv_click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
+}
+
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
+}
+
+static void set_status_bar(Layer *layer) {
+  // Create the StatusBarLayer
+  s_status_bar = status_bar_layer_create();
+
+  // Set properties
+  status_bar_layer_set_separator_mode(s_status_bar, StatusBarLayerSeparatorModeDotted);
+
+  // Add to Window
+  layer_add_child(layer, status_bar_layer_get_layer(s_status_bar));
+}
+
+static void set_action_bar_layer(Window *window) {
+  // Load icon bitmaps
+  s_up_bitmap = gbitmap_create_with_resource(RESOURCE_ID_UP_ICON);
+  s_down_bitmap = gbitmap_create_with_resource(RESOURCE_ID_DOWN_ICON);
+  s_check_bitmap = gbitmap_create_with_resource(RESOURCE_ID_CHECK_ICON);
+
+  // Create ActionBarLayer
+  s_action_bar = action_bar_layer_create();
+  action_bar_layer_set_click_config_provider(s_action_bar, click_config_provider);
+
+  // Set the icons
+  action_bar_layer_set_icon(s_action_bar, BUTTON_ID_UP, s_up_bitmap);
+  action_bar_layer_set_icon(s_action_bar, BUTTON_ID_DOWN, s_down_bitmap);
+  action_bar_layer_set_icon(s_action_bar, BUTTON_ID_SELECT, s_check_bitmap);
+
+  // Add to Window
+  action_bar_layer_add_to_window(s_action_bar, window);
+}
+
+static void prv_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+
+  set_status_bar(window_layer);
+
+  set_action_bar_layer(window);
+
+  GRect bounds = layer_get_bounds(window_layer);
+
+  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
+
+  if (app_worker_is_running()) {
+    text_layer_set_text(s_text_layer, "Worker is running");
+  } else {
+    text_layer_set_text(s_text_layer, "Press a button");
+  }
+  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+}
+
+static void prv_window_unload(Window *window) {
+  // Destroy the TextLayer
+  text_layer_destroy(s_text_layer);
+
+  // Destroy the StatusBarLayer
+  status_bar_layer_destroy(s_status_bar);
+}
+
+static void prv_init(void) {
+  s_window = window_create();
+  window_set_click_config_provider(s_window, prv_click_config_provider);
+  window_set_window_handlers(s_window, (WindowHandlers) {
+    .load = prv_window_load,
+    .unload = prv_window_unload,
+  });
+  const bool animated = true;
+  window_stack_push(s_window, animated);
+  app_worker_message_subscribe(worker_message_handler);
+
+  if (!app_worker_is_running()) {
+    // Data logging
+	  data_logging_finish(data_logging_create(LOGGING_TAG, DATA_LOGGING_INT, sizeof(int16_t), false));
+  }
+}
+
+static void prv_deinit(void) {
+  window_destroy(s_window);
+  app_worker_message_unsubscribe();
+}
+
+int main(void) {
+  prv_init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_window);
+
+  app_event_loop();
+  prv_deinit();
+}
---
+++ worker_src/c/swp_worker.c
@@ -0,0 +1,85 @@
+#include <pebble_worker.h>
+
+#ifndef MESSAGE_SOURCES
+#define MESSAGE_SOURCES
+#define SOURCE_FOREGROUND 0
+#define SOURCE_BACKGROUND 1
+#endif
+
+#ifndef LOGGING_TAG
+#define LOGGING_TAG 0xFEDE1983
+#endif
+
+#define NUM_SAMPLES 10
+
+// Session logging
+static DataLoggingSessionRef s_session_ref;
+
+static void worker_message_handler(uint16_t type, AppWorkerMessage *message) {
+  if (type == SOURCE_FOREGROUND) {
+    // ya veremos lo que hacemos
+  }
+}
+
+static void send_init_message() {
+  AppWorkerMessage message = {
+    .data0 = 0xFFFF,
+    .data1 = 0xFFFF,
+    .data2 = 0xFFFF
+  };
+  app_worker_send_message(SOURCE_BACKGROUND, &message);
+}
+
+static void send_deinit_message() {
+  AppWorkerMessage message = {
+    .data0 = 0x0000,
+    .data1 = 0x0000,
+    .data2 = 0x0000
+  };
+  app_worker_send_message(SOURCE_BACKGROUND, &message);
+}
+
+static void accel_data_handler(AccelData *data, uint32_t num_samples) {
+  uint32_t i;
+	for (i=0; i < num_samples; i++) {
+		if (!data[i].did_vibrate) {
+			int16_t data_to_log[3];
+			data_to_log[0] = data[i].x;
+			data_to_log[1] = data[i].y;
+			data_to_log[2] = data[i].z;
+			if (data_logging_log(s_session_ref, &data_to_log, 3) != DATA_LOGGING_SUCCESS) {
+				APP_LOG(APP_LOG_LEVEL_DEBUG, "DATA LOGGING ERROR.");
+			}
+		}
+	}
+}
+
+static void prv_init() {
+  // Initialize the worker here
+  app_worker_message_subscribe(worker_message_handler);
+  send_init_message();
+
+  // Data logging
+	s_session_ref = data_logging_create(LOGGING_TAG, DATA_LOGGING_INT, sizeof(int16_t), true);
+  accel_service_set_sampling_rate(ACCEL_SAMPLING_50HZ);
+	// Subscribe to batched data events
+	accel_data_service_subscribe(NUM_SAMPLES, accel_data_handler);
+}
+
+static void prv_deinit() {
+  // Deinitialize the worker here
+  send_deinit_message();
+  app_worker_message_unsubscribe();
+
+  // Unsuscribe accel data service
+	accel_data_service_unsubscribe();
+
+	// Finish data loggin
+	data_logging_finish(s_session_ref);
+}
+
+int main(void) {
+  prv_init();
+  worker_event_loop();
+  prv_deinit();
+}
